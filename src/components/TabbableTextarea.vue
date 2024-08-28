2cc69934587f7e46cd363687ad054607c3097b17
012kirby210 <012kirby210@gmail.com>
2024-08-28 10:06:23 +0200

    Définition du composant TabbableTextarea.

---
+++ src/components/TabbableTextarea.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+defineProps({
+  modelValue: String
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+function onTabPress(e) {
+  let textarea = e.target;
+
+
+  let val = textarea.value,
+      start = textarea.selectionStart,
+      end = textarea.selectionEnd;
+
+  textarea.value = val.substring(0,start) + "\t" + val.substring(end);
+
+  textarea.selectionStart = textarea.selectionEnd = start + 1;
+
+}
+
+</script>
+
+<template>
+  <textarea
+      @keydown.tab.prevent="onTabPress"
+      @keyup="emit('update:modelValue',String($event.target.value))"
+      v-text="modelValue" />
+</template>
+
+<style scoped>
+
+</style>
