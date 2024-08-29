ffc836b1ed5ffdbf5305c7a64daf2fad89b960dd
012kirby210 <012kirby210@gmail.com>
2024-08-29 15:23:42 +0200

    Extract State as a reactive into a file.

--- src/components/Quiz/QuizFooterLinks.vue
+++ src/components/Quiz/QuizFooterLinks.vue
@@ -1,12 +1,11 @@
 <script setup>
-  import {inject} from "vue";
-
-  let quiz = inject('quiz');
+  import {state} from "@/Stores/quizStore.js";
 
 </script>
 
 <template>
-  <div>{{quiz.name}}</div>
+  <div>{{state.name}}</div>
+  <button @click.prevent="state.name = 'some new name'">Change Quiz name</button>
   <ul>
     <li><a href="#">Get a job</a></li>
     <li><a href="#">Contact us</a></li>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,14 @@
 <script setup>
-  // import TabbableTextarea from "@/components/TabbableTextarea.vue";
-  // import {ref} from "vue";
+
   import QuizLayout from "@/components/Quiz/QuizLayout.vue";
 
-  // let comment = ref("test value");
 </script>
 
 
 <template>
   <main>
     <form>
-<!--      <TabbableTextarea v-model="comment" style="width: 100%; height: 300px" />-->
-    <QuizLayout :quiz="{name: 'My First Quiz', questions: []}" />
+    <QuizLayout />
     </form>
   </main>
 </template>
