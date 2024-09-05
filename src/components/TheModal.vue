25117cf2651bd9ee3baad9da0280bb96d4eae54f
012kirby210 <012kirby210@gmail.com>
2024-09-05 19:44:30 +0200

    Style et slotting de la modale.

--- src/components/TheModal.vue
+++ src/components/TheModal.vue
@@ -8,13 +8,15 @@
   <div class="modal-mask" v-if="show">
     <div class="modal-container">
       <header>
-        header
+        <slot name="headaer">default header</slot>
       </header>
       <div>
-        Main
+        <slot name="default">Main content</slot>
       </div>
       <footer>
-        Footer
+        <slot name="footer">
+          <button @click="$emit('close')">Close</button>
+        </slot>
       </footer>
     </div>
   </div>
@@ -34,5 +36,22 @@
   padding: 1rem;
   width: 80vw;
   max-width: 500px;
+  border-radius: 7px;
+}
+.modal-container footer{
+  font-size: 14px;
+  border-top: 1px solid var(--vt-c-black-mute);
+  padding-top: 5px;
+}
+footer button{
+  background: var(--vt-c-black-mute);
+  border-radius: 20px;
+  padding: 0 1rem;
+}
+footer button:hover{
+  background: var(--vt-c-black-soft);
+}
+.modal-container header + div {
+  padding-bottom: 1rem;
 }
 </style>
