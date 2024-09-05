b3f23a0a168ba6407d321fea2939b10e544836fc
012kirby210 <012kirby210@gmail.com>
2024-09-05 19:24:48 +0200

    Ajout du composant modal.

---
+++ src/components/TheModal.vue
@@ -0,0 +1,38 @@
+<script setup>
+  defineProps({
+    show: Boolean,
+  });
+</script>
+
+<template>
+  <div class="modal-mask" v-if="show">
+    <div class="modal-container">
+      <header>
+        header
+      </header>
+      <div>
+        Main
+      </div>
+      <footer>
+        Footer
+      </footer>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+.modal-mask{
+  position: fixed;
+  inset: 0;
+  background: #00000088;
+  display: grid;
+  place-items: center;
+}
+.modal-container{
+  background: var(--vt-c-black);
+  padding: 1rem;
+  width: 80vw;
+  max-width: 500px;
+}
+</style>
